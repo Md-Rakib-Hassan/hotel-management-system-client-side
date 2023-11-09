723fea8f68a37b03c6a4b24c085d59cb0876acb4
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-11-09 13:09:04 +0600

    Complete the About us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 const AboutUs = () => {
     return (
-        <div>
-            
-        </div>
+        <div className="container mx-auto p-4">
+            <h1 className='text-center font-black lg:text-5xl md:text-4xl text-2xl pt-8 pb-4  mb-4'>About Us</h1>
+       
+        <p className="text-gray-700 leading-relaxed">
+          Welcome to our hotel booking website. We take pride in offering you the best hotel booking experience. Our mission is to ensure that your journey is filled with comfort, convenience, and affordability.
+        </p>
+        <p className="text-gray-700 leading-relaxed mt-4">
+          At our hotel, we understand that every traveler has unique needs and preferences. That's why we provide a diverse range of accommodation options to cater to you. Whether you're a solo adventurer, a family explorer, or a business traveler, we have the ideal room for your stay.
+        </p>
+        <p className="text-gray-700 leading-relaxed mt-4">
+          Our well-furnished rooms are designed with your comfort in mind. From the moment you arrive, our dedicated staff is here to ensure your stay is nothing short of excellent. We are committed to delivering top-notch customer service and maintaining competitive pricing, so you get the best value for your money.
+        </p>
+        <p className="text-gray-700 leading-relaxed mt-4">
+          Explore our website to discover the various room options, check availability, and make your reservations with ease. Whether you're planning a relaxing vacation, a productive business trip, or a memorable family getaway, we have the perfect space for your needs. We look forward to hosting you on your next journey and making it unforgettable.
+        </p>
+      </div>
     );
 };
 
