fd24132b719daee38b05177316fcae16e8f4d5ed
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-11-09 15:54:20 +0600

    Complete the Career Opportunities Page

--- src/pages/CareerOpportunities.jsx
+++ src/pages/CareerOpportunities.jsx
@@ -1,9 +1,128 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import useAxios from '../customHooks/useAxios';
+import Swal from 'sweetalert2';
+import { AuthContext } from '../provider/AuthProvider';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const CareerOpportunities = () => {
+
+    const axios = useAxios();
+    const [jobData, setJobData] = useState([]);
+    const {user}=useContext(AuthContext);
+    
+
+    useEffect(() => {
+        axios.get('/jobsopen')
+            .then(res => setJobData(res.data));
+    }, [axios]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const cvlink = e.target.cvlink.value
+        const name = e.target.name.value
+        const email = e.target.email.value
+
+
+        const cvInfo={name,email,cvlink};
+        
+
+        axios.post('/job-req',cvInfo)
+        .then(res=>{
+            if(res.data.acknowledged){
+            e.target.reset();
+            return toast.success("We will contact you soon")
+        
+
+            }
+            return toast.error("We will contact you soon")
+             
+
+        })
+
+
+
+    }
+
+
     return (
-        <div>
-            
+        <div className="container mx-auto p-4 mt-10">
+            <ToastContainer />
+            <h1 className="text-3xl font-bold text-center mb-6">Career Opportunities at BookHotel</h1>
+            <p className="text-gray-700 mb-6">
+                Join our team at BookHotel and be part of an exciting journey in the hospitality industry. Explore the current career opportunities below and discover how you can contribute to creating memorable experiences for our guests.
+            </p>
+
+            <section className="mb-8 flex flex-col-reverse lg:flex-row">
+
+                <div className='flex-grow lg:w-2/3'>
+                    <h2 className="text-2xl font-semibold mb-4">Current Openings</h2>
+                    {
+                        jobData.map((job, index) => <div key={job._id} className="mb-4">
+
+                            <h3 className="text-xl font-semibold">{index + 1}. {job.title}</h3>
+                            <p className="text-gray-700">{job.description}</p>
+                            {/* Open the modal using document.getElementById('ID').showModal() method */}
+                            <button className="p-.5 text-white px-2 rounded-md bg-blue-500" onClick={() => document.getElementById(job._id).showModal()}>Apply Here</button>
+                            <dialog id={job._id} className="modal">
+                                <div className="modal-box">
+
+
+                                    <form onSubmit={handleSubmit} className='space-y-10'>
+
+                                    <input name='cvlink' disabled value={job?.title} required placeholder='Give your Cv link' className=' w-full border-b border-blue-300 focus:outline-none py-1 focus:border-blue-800 focus:border-b-2 transition-colors' type="text" />
+                                    <input name='name' disabled value={user?.displayName} required placeholder='Name' className=' w-full border-b border-blue-300 focus:outline-none py-1 focus:border-blue-800 focus:border-b-2 transition-colors' type="text"  />
+                <input name='email' disabled value={user?.email} required placeholder='Email' className='w-full border-b border-blue-300 focus:outline-none py-1 focus:border-blue-800 focus:border-b-2 transition-colors' type="email"  />
+                                        <input name='cvlink'  required placeholder='Give your Cv link' className=' w-full border-b border-blue-300 focus:outline-none py-1 focus:border-blue-800 focus:border-b-2 transition-colors' type="text" />
+                                        
+                                        <input className='btn bg-blue-500 hover:bg-blue-600 text-white' type="submit" value={'SUBMIT'} />
+                                    </form>
+
+
+
+                                </div>
+                                <form method="dialog" className="modal-backdrop">
+                                    <button>close</button>
+                                </form>
+                            </dialog>
+                        </div>)
+
+                    }
+
+                </div>
+
+                <div>
+                    <img className='flex-grow' src="https://i.ibb.co/Mch4X2m/undraw-Job-hunt-re-q203.png" alt="" />
+                </div>
+
+
+            </section>
+
+            <section className="mb-8">
+                <h2 className="text-2xl font-semibold mb-4">Why Join BookHotel?</h2>
+                <p className="text-gray-700">
+                    At BookHotel, we value collaboration, innovation, and a commitment to exceptional service. Joining our team means being part of a dynamic and supportive work environment. Here are some reasons to consider a career with us:
+                </p>
+                <ul className="list-disc list-inside text-gray-700">
+                    <li>Opportunities for professional growth and development</li>
+                    <li>A positive and inclusive workplace culture</li>
+                    <li>Employee discounts on accommodation and dining</li>
+
+                </ul>
+            </section>
+
+            <section className="mb-8">
+                <h2 className="text-2xl font-semibold mb-4">How to Apply</h2>
+                <p className="text-gray-700">
+                    To apply for any of the positions listed above, please follow these steps:
+                </p>
+                <ol className="list-decimal list-inside text-gray-700">
+                    <li>Review the job description and requirements carefully.</li>
+                    <li>Prepare your resume and a cover letter highlighting your relevant experience.</li>
+                    <li>Submit your application through our online application form.</li>
+
+                </ol>
+            </section>
         </div>
     );
 };
--- src/pages/LocalAreaGuide.jsx
+++ src/pages/LocalAreaGuide.jsx
@@ -1,11 +1,107 @@
-import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
+import Swal from 'sweetalert2';
+import useAxios from '../customHooks/useAxios';
 
 const LocalAreaGuide = () => {
-    return (
-        <div>
+
+    const {user}=useContext(AuthContext);
+    const axios=useAxios();
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const place = e.target.place.value
+        const requirement = e.target.requirement.value
+
+        const guideReqInfo={name,email,place,requirement};
+
+        axios.post('/local-guide-req',guideReqInfo)
+        .then(res=>{
+            if(res.data.acknowledged){
+            e.target.reset();
+             return   Swal.fire('Done', 'We will managing your guide as your requirement. ', 'success')
+
+            }
+             return   Swal.fire('Try Again', 'Something gone wrong', 'error')
+             
+
+        })
+
+    
+
+    }
+
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold text-center mb-4">Local Area Guide</h1>
+      <p className="text-gray-700 mb-6">
+        Welcome to our Local Area Guide! Explore the surrounding area and discover the best attractions and activities. Whether you&apos;re interested in sightseeing, dining, outdoor adventures, or cultural experiences, there&apos;s something for everyone to enjoy. Check out our recommendations below.
+      </p>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2">Sightseeing</h2>
+        <ul className="list-disc list-inside text-gray-700">
+          <li>Visit the local historic landmarks and monuments.</li>
+          <li>Explore scenic parks and gardens with stunning views.</li>
+          <li>Take a guided city tour to learn about the area&apos;s history.</li>
+        </ul>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2">Dining</h2>
+        <ul className="list-disc list-inside text-gray-700">
+          <li>Experience local cuisine at the finest restaurants.</li>
+          <li>Discover popular cafes and bakeries for a quick bite.</li>
+          <li>Don&apos;t miss the street food and food markets for local flavors.</li>
+        </ul>
+      </section>
+
+
+        <div className='mt-24'>
+
+        <div className='w-3/4 mx-auto space-y-3'>
+            <div className='mt-10'>
+
+            <div className='mb-10'>
+            <h1 className='font-bold text-2xl text-center'>Need Local Guide?</h1>
+            <p className='text-gray-500 text-center'>Do you feel like you need a local guide to explore the city? Then don&apos;t waste time fill out the form now. We will manage a local guide for you.</p>
+
+            </div>
+
             
+
+            <form onSubmit={handleSubmit}  className='space-y-10'>
+               <div className='flex flex-col md:flex-row w-full gap-8'>
+  
+                <input name='name' disabled value={user?.displayName} required placeholder='Name' className=' w-full border-b border-blue-300 focus:outline-none py-1 focus:border-blue-800 focus:border-b-2 transition-colors' type="text"  />
+                <input name='email' disabled value={user?.email} required placeholder='Email' className='w-full border-b border-blue-300 focus:outline-none py-1 focus:border-blue-800 focus:border-b-2 transition-colors' type="email"  />
+     
+               </div>
+               <input name='place' required placeholder='Where you want to visit?' className='w-full border-b border-blue-300 focus:outline-none py-1 focus:border-blue-800 focus:border-b-2 transition-colors' type="text"  />
+
+               <textarea name='requirement' required placeholder='Do you have any specific requirement?' className='w-full border-b border-blue-300 focus:outline-none py-1 focus:border-blue-800 focus:border-b-2 transition-colors' type="text"  />
+               <div className='flex justify-center'>
+               <input className='btn bg-blue-500 hover:bg-blue-600 text-white' type="submit" value={'SUBMIT'} />
+
+               </div>
+               
+
+            </form>
+
+            </div>
+            
+           
         </div>
-    );
+        </div>
+
+      
+
+      
+    </div>
+  );
 };
 
 export default LocalAreaGuide;
