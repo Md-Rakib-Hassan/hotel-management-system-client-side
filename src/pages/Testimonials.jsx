89017af3501f9a646b1dc803f8305d46dfe6319f
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2023-11-13 00:08:57 +0600

    fixed the tesimonial page testimonials are overlaps problem on phone

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -18,7 +18,7 @@ const Testimonials = () => {
         <div className=' py-24 overflow-hidden' style={{background: 'linear-gradient(90deg, rgba(2,0,36,0.5) 0%, rgba(9,9,121,0.5) 35%, rgba(0,212,255,.5) 100%)'}} >
             <div className='mx-auto'>
 
-            {reviews.map((review ,indx)=><div key={review._id} className={`flex justify-around  ${indx%2 ? 'flex-row':'md:flex-row-reverse'}`}>
+            {reviews.map((review ,indx)=><div key={review._id} className={`flex justify-around md:mb-1 mb-16 ${indx%2 ? 'md:flex-row':'md:flex-row-reverse'}`}>
 
                 <div className={`w-72 mb-18 `} data-aos={`${indx%2 ? 'fade-down-right' : 'fade-down-left'}`}  ><Review  info={review}></Review> </div>
                 <div className='w-72 md:inline hidden'></div>
